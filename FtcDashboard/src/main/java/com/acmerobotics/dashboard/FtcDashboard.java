2e935144ab62e3d11be20cc5620aa0fb921610d1
Nash Pillai <nash.pillai@protonmail.com>
2023-01-10 23:19:41 -0500

    remove debug logging

--- FtcDashboard/src/main/java/com/acmerobotics/dashboard/FtcDashboard.java
+++ FtcDashboard/src/main/java/com/acmerobotics/dashboard/FtcDashboard.java
@@ -294,7 +294,7 @@ public class FtcDashboard implements OpModeManagerImpl.Notifications {
                             fieldOverlay.strokeCircle(pose.getX(), pose.getY(), 3);
                         }
                     }
-                    Log.d(TAG, "Sending Telementry Packet - " + uploadedPath);
+//                    Log.d(TAG, "Sending Telementry Packet - " + uploadedPath);
 
                     sendAll(new ReceiveTelemetry(telemetryToSend));
 
